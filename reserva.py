9835bf4d4bf2afffbadd88855c86d0994ea926d6
jvictormorel <jvmorelrodrigues@gmail.com>
2024-10-23 20:18:25 -0300

    adicionando mais funcionalidades, botão de user/adm e email e senha

--- reserva.py
+++ reserva.py
@@ -1,23 +1,52 @@
-import tkinter
-from tkinter import Tk, ttk, PhotoImage
+import tkinter as tk
+from tkinter import ttk
 
+# cores
 cor_preta = "#f0f3f5" 
 cor_branca = "#f3ffff"
 cor_verde = "#3fb5a3" 
 valr = "#38576b" 
 letr = "#403d3d" 
 
+# Funções para login como admin ou usuário comum
+def login_admin():
+    print("Administrador")
+    
 
-# janela de login
+def login_usuario():
+    print("Usuário Comum")
 
-tela_login = Tk()
-tela_login.title('')
-tela_login.geometry('400x400')
-tela_login.configure(background=cor_branca)
-tela_login.resizable(width=False, height=False)
+# tela de login
+def criar_tela_login():
+    tela_login = tk.Tk()
+    tela_login.title('Tela de Login')
+    tela_login.geometry('600x400')
+    tela_login.configure(background=cor_verde)
+    tela_login.resizable(width=False, height=False)
 
-tela_login.mainloop()
+    
+    titulo = tk.Label(tela_login, text="Reserva de materiais (IME)", font=('Arial', 18), bg=cor_verde, fg=letr)
+    titulo.pack(pady=20)
 
+    # entradas de e-mail e senha
+    label_usuario = tk.Label(tela_login, text="E-Mail", bg=cor_verde, fg=letr, font=('Arial', 12))
+    label_usuario.pack(pady=5)
+    entrada_usuario = tk.Entry(tela_login, width=30)
+    entrada_usuario.pack(pady=5)
 
+    label_senha = tk.Label(tela_login, text="Senha", bg=cor_verde, fg=letr, font=('Arial', 12))
+    label_senha.pack(pady=5)
+    entrada_senha = tk.Entry(tela_login, show="*", width=30)
+    entrada_senha.pack(pady=5)
 
+    # botão de adm e usuário comum
+    botao_admin = ttk.Button(tela_login, text="Login como Admin", command=login_admin)
+    botao_admin.pack(pady=10)
 
+    botao_usuario = ttk.Button(tela_login, text="Login como Usuário Comum", command=login_usuario)
+    botao_usuario.pack(pady=10)
+
+    tela_login.mainloop()
+
+# tela de login
+criar_tela_login()
