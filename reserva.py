c8824bd59c439be9adde6452d2afa29b80b86bb6
jvictormorel <jvmorelrodrigues@gmail.com>
2024-10-23 10:56:10 -0300

    tela criada (em branco ainda)

---
+++ reserva.py
@@ -0,0 +1,23 @@
+import tkinter
+from tkinter import Tk, ttk, PhotoImage
+
+cor_preta = "#f0f3f5" 
+cor_branca = "#f3ffff"
+cor_verde = "#3fb5a3" 
+valr = "#38576b" 
+letr = "#403d3d" 
+
+
+# janela de login
+
+tela_login = Tk()
+tela_login.title('')
+tela_login.geometry('400x400')
+tela_login.configure(background=cor_branca)
+tela_login.resizable(width=False, height=False)
+
+tela_login.mainloop()
+
+
+
+
