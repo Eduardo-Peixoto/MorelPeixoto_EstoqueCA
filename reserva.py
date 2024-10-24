e3f7c4d89b50815fa25d740816e4197cb08977c4
jvictormorel <jvmorelrodrigues@gmail.com>
2024-10-23 21:56:16 -0300

    área de login feita (só a tela)

--- reserva.py
+++ reserva.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, PhotoImage
+from PIL import Image, ImageTk
 
 # cores
 cor_preta = "#f0f3f5" 
@@ -8,7 +9,6 @@ cor_verde = "#3fb5a3"
 valr = "#38576b" 
 letr = "#403d3d" 
 
-# Funções para login como admin ou usuário comum
 def login_admin():
     print("Administrador")
     
@@ -27,6 +27,20 @@ def criar_tela_login():
     
     titulo = tk.Label(tela_login, text="Reserva de materiais (IME)", font=('Arial', 18), bg=cor_verde, fg=letr)
     titulo.pack(pady=20)
+    
+    img1 = Image.open("C:/Users/jvmor/OneDrive/Área de Trabalho/proje_reserva_materiais_CA/MorelPeixoto_EstoqueCA/simbolo_eb.png")
+    img1 = img1.resize((100, 120), Image.Resampling.LANCZOS) 
+    imagem1 = ImageTk.PhotoImage(img1) 
+    
+    img2 = Image.open("C:/Users/jvmor/OneDrive/Área de Trabalho/proje_reserva_materiais_CA/MorelPeixoto_EstoqueCA/simbolo_ime.png")
+    img2 = img2.resize((100, 120), Image.Resampling.LANCZOS) 
+    imagem2 = ImageTk.PhotoImage(img2)
+    
+    img_label1 = tk.Label(tela_login, image=imagem1, bg=cor_verde)
+    img_label1.place(x=0, y=0)
+
+    img_label2 = tk.Label(tela_login, image=imagem2, bg=cor_verde)
+    img_label2.place(x=500, y=0)
 
     # entradas de e-mail e senha
     label_usuario = tk.Label(tela_login, text="E-Mail", bg=cor_verde, fg=letr, font=('Arial', 12))
